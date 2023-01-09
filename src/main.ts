d0de6c0d231f761cd3b33c417a787e6090f09a4e
Michael Gomes <seed@DESKTOP-QSDDVNS>
2023-01-08 23:42:30 -0300

    commit until chapter 22 page 79

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,10 @@
 import { NestFactory } from '@nestjs/core';
-import { AppModule } from './app.module';
 import { NestExpressApplication } from '@nestjs/platform-express';
-import { join } from 'path';
+import session from 'express-session';
 import * as hbs from 'hbs';
 import * as hbsUtils from 'hbs-utils';
+import { join } from 'path';
+import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create<NestExpressApplication>(AppModule);
@@ -13,6 +14,17 @@ async function bootstrap() {
   hbs.registerPartials(join(__dirname, '..', 'views/layouts'));
   hbsUtils(hbs).registerWatchedPartials(join(__dirname, '..', 'views/layouts'));
   app.setViewEngine('hbs');
+  app.use(
+    session({
+      secret: 'nest-book',
+      resave: false,
+      saveUninitialized: false,
+    }),
+  );
+  app.use((req, res, next) => {
+    res.local.session = req.session;
+    next();
+  })
 
   await app.listen(3000);
 }
